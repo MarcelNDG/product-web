7c32c81f7fe6f9b60116a605977b6ebafa77be41
Marcel <marcelnunez.dg@gmail.com>
2022-10-24 19:25:25 -0300

    files commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let myArray = [1,2,3,4,"five"]
+
+anotherArray = [4,5,"six"]
+
+console.log(myArray, anotherArray);
+
+if (myArray[0] < 4) {
+    myArray.push("kek")
+    anotherArray.push(6)
+    console.log("hola");
+    myArray.sort(String)
+    console.log(myArray, anotherArray);
+} else {
+    
+}
