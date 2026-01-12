77b139fccb95bdc5f411b549c6ac60333813685d
MarcelNDG <69280401+MarcelNDG@users.noreply.github.com>
2026-01-12 19:40:17 -0300

    Navbar deactivate fix

--- script.js
+++ script.js
@@ -4,10 +4,35 @@ let mobileBtn = document.getElementById("mobileBtn");
 let mobileMenu = document.getElementById("mobileMenu");
 let body = document.getElementById("body");
 
+let menuLinkSetter = menuLinks.querySelectorAll("a");
+
+
+
 const navBar = document.getElementById("mainNavbar");
 const stickyOffset = navBar.offsetTop;
 
 console.log(screenX);
+
+function Iterate() {
+    menuLinkSetter.forEach(function(linkItem){
+        // to add an id to each link item what must be done is to use the add method of the classList property
+        linkItem.classList.add("menuLink2");
+
+        linkItem.addEventListener("click", function(){
+            mobileMenu.classList.remove("active")
+            
+        })  
+    })
+}
+
+Iterate();
+
+console.log(menuLinkSetter);
+console.log(menuLink);
+
+
+
+
 
 /* let myArray = [1,2,3,4,"five"]
 
@@ -26,13 +51,16 @@ if (myArray[0] < 4) {
 }*/
 
 menuBtn.addEventListener("click", function click() {
-    console.log("holo");
+    console.log("activado");
    // mobileMenu.style.display = "flex"; 
     mobileMenu.classList.add("active") 
 })
 
+
+
+
 mobileBtn.addEventListener("click", function click() {
-    console.log("holo"); 
+    console.log("desactivado"); 
    // mobileMenu.style.display = "none"
     mobileMenu.classList.remove("active") 
 })
